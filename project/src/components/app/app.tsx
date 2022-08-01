10075bdc1c460bc3e508f19d0fa4c3a782530e8c
Dmitriy Kozlov <mr.dimk@icloud.com>
2022-08-01 23:50:16 +0300

    Первичное деление на страницы

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,12 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import { Offer } from '../../mocks/offers';
+import MainPage from '../../pages/main/main';
+
+type AppProps = {
+  offers: Offer[],
+};
+
+function App({ offers }: AppProps): JSX.Element {
+  return <MainPage offers={offers} />;
 }
 
 export default App;
